3a29cf36f8a581c97b74020371efff445554adbe
Lucas Yan <164198570+LucasYaan@users.noreply.github.com>
2025-02-27 01:20:12 +0000

    feat: aula 3

---
+++ aula_03/Imagine.java
@@ -0,0 +1,31 @@
+package aula_03;
+
+import java.util.Scanner;
+
+public class Imagine {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Quantas publicações você quer analisar: ");
+        int numPubs = entrada.nextInt();
+        int totalLikes = 0;
+
+        for (int i = 1; i <= numPubs; i++) {
+            System.out.printf("Quantas curtidas essa publicação tem? #%d", 1); // printf e %d
+            int likes = entrada.nextInt();
+            totalLikes += likes;
+                if (likes >= 100) {
+                    System.out.println("Publicação Popular!");
+                } else {
+                    System.out.println("Publicação com poucas curtidas.");
+                }
+        }
+
+        double avgLikes = (double) totalLikes / numPubs;
+        System.out.printf("Esta é a média de curtidas das publicações = %.1f\n", avgLikes);
+        System.out.print("Número de publicações = ");
+            
+        entrada.close();
+    }
+    
+}
