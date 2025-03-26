4eb610fa2a69ead4a43d5aa29a1c358e4e193e12
Lucas Yan <164198570+LucasYaan@users.noreply.github.com>
2025-03-26 18:59:22 +0000

    feat: adding first exercises

---
+++ listas/lista_01/Intro3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Intro2 {
+      public static void main(String[] args) {
+    Scanner entrada = new Scanner(System.in);
+
+    // Solicite ao usuário o seu nome completo e escreva uma saudação para ele.
+    System.out.print("Digite o seu nome completo: ");
+    String nomeCompleto = entrada.nextLine();
+
+    System.out.println("Olá, " + nomeCompleto + ", é um prazer conhecê-lo.");
+
+    entrada.close();
+  }
+    
+}
